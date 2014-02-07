6ce5d536004fa4f9fe28f3e862043409abdc4773
Jean-Marie Dautelle <dautelle@gmail.com>
2014-02-07 18:36:43 +0000

    Constant Annotation

--- javolution/core-java/src/main/java/javolution/lang/Constant.java
+++ javolution/core-java/src/main/java/javolution/lang/Constant.java
@@ -16,17 +16,29 @@ import java.lang.annotation.RetentionPolicy;
 import java.lang.annotation.Target;
 
 /**
- * <p> Indicates that the state of a class instance, a method return value, 
- *     a static field or a method parameter should not change after creation 
- *     or method invocation (for method return value or method parameter).
+ * <p> Indicates that the state of a class instance, a static field or a 
+ *     method return value / parameter will not change ever. For parameters 
+ *     tagged {@code Constant}, defensive copy is unnecessary. 
  * [code]
- * @Constant
- * public class ConstantTable<F> extends FastTable<F> {
- *     public static <E> ConstantTable<E> of(@Constant E... elements ) { ... }
- *     public @Constant unmodifiable() { return this; } 
- *     ...
- * }
+ * @Constant(comment="Immutable")
+ * class Polygon extends Shape  { 
+ *     private Point2D[] vertices;
+ *     public Polygon(@Constant Point2D... vertices) { 
+ *         this.vertices = vertices; // No defensive copying required.
+ *     }
+ *     @Constant(comment="Unmodifiable View") 
+ *     List<Point2D> getVertices() { 
+ *         return ConstantTable.of(vertices); // Unmodifiable array wrapper. 
+ *     }
+ * }[/code]</p>
  * 
+ * <p> The constant annotation is primarily for API documentation purpose but 
+ *     static analyzers could also be used to detect constant rules violations. 
+ * [code]
+ * Polygon triangle = new Polygon(p1, p2, p3); // Ok, literals are always constant.
+ * Point2D[] vertices = new Point2D[] { p1, p2, p3 };
+ * triangle = new Polygon(vertices); // vertices is now assumed constant.
+ * vertices[0] = null; // Rule violation, modification after vertices has been assumed constant!    
  * [/code]
  *   
  * @author  <a href="mailto:jean-marie@dautelle.com">Jean-Marie Dautelle</a>
@@ -36,7 +48,7 @@ import java.lang.annotation.Target;
 @Inherited
 @Target({ ElementType.TYPE, ElementType.FIELD, ElementType.METHOD, ElementType.PARAMETER,
 	       ElementType.LOCAL_VARIABLE})
-@Retention(RetentionPolicy.SOURCE)
+@Retention(RetentionPolicy.CLASS)
 public @interface Constant  {
 
     /**
